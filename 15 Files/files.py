b1dee42b6267e83806d6002aa22ed647f08e9a98
zhjohn925 <zhjohn925@gmail.com>
2020-11-07 11:57:03 -0800

    Update files.py

--- 15 Files/files.py
+++ 15 Files/files.py
@@ -1,11 +1,62 @@
 #!/usr/bin/python3
-# files.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
 
 def main():
+    #'r': read mode as default 
+    #'rt': text mode; 'rb': binary mode
+    #'w': write; 'a': append; 'r+': read and write
+    #f is file object
     f = open('lines.txt')
     for line in f:
         print(line, end = '')
+        
+def main1():
+    f = open('lines.txt', 'r')
+    for line in f.readlines():
+        print(line, end = '')
+    print('Done.')
+        
+#copy file        
+def main2():
+    infile = open('lines.txt', 'r')
+    outfile = open('new.txt', 'w')
+    for line in infile:
+        print(line, file=outfile, end = '')
+    print('Done.')    
+        
+#large file. do not want to read line by line to copy
+#copy file        
+def main3():
+    buffersize = 50000
+    infile = open('bigfile.txt', 'r')
+    outfile = open('new.txt', 'w')
+    buffer = infile.read(buffersize)
+    while len(buffer) :
+        outfile.write(buffer)
+        print('.', end='')
+        buffer = infile.read(buffersize)
+    print()
+    print('Done.')
+    
+#copy binary file, Bad demo
+def main4():
+    f = open('olives.jpg')
+    for line in f:
+        print(line, end='')
+        
+#copy binary file ie. image files
+def main5():        
+    buffersize = 50000
+    infile = open('olives.jpg', 'rb')
+    outfile = open('new.jpg', 'wb')
+    buffer = infile.read(buffersize)
+    while len(buffer) :
+        outfile.write(buffer)
+        print('.', end='')
+        buffer = infile.read(buffersize)
+    print()
+    print('Done.')
+
 
 if __name__ == "__main__": main()
+    
+
