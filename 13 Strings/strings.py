65d179f6d7cda527b09f63ecc9b9df48421ac410
zhjohn925 <zhjohn925@gmail.com>
2020-11-07 17:08:42 -0800

    Update strings.py

--- 13 Strings/strings.py
+++ 13 Strings/strings.py
@@ -7,6 +7,8 @@ def main():
     #string variable is a reference to the string
     s = 'this is a string'
     print(s.upper())
+    print(s.lower())
+    print(s.swapcase())
     print(s.capitalize())
     print(s.title())
     print(s.upper())
@@ -21,5 +23,26 @@ def main():
     'This is a string'.upper()
     'This is a string {}'.format(42)
     'This is a string %d' % 42
+    s3 = 'this is {}, that is {}'
+    s3.format(a, b)
+    s3 = 'this is {1}, that is {0}'   #specify positional order
+    s3.format(a, b)
+    'this is {bob} and that is {fred}'.format(bob=a, fred=b)
+    d = dict(bob=a, fred=b)  #apply dictionary
+    'this is {bob} and that is {fred}'.format(**d)
+    s.find('is')
+    s.replace('this', 'that')  #remember string is immutable
+    id(s)
+    newstring = s.upper()
+    id(newstring)
+    ' This is a string '.strip()  #remove white space, include new-line
+    ' This is a string '.rstrip() #remove white space on the right
+    s1 = 'This is a string \n'
+    s1.rstrip('\n')
+    s1.isalnum()  #False since there are space in s1
+    'thisisastring'.isalnum() #True
+    'thisisastring'.isalpha() #True
+    '12345'.isdigit() #True
+    s1.isprintable() #True  
 
 if __name__ == "__main__": main()
