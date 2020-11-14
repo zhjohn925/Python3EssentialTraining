11d756eea1091d0de7e114eb85ac4b6a9f0a4049
zhjohn925 <zhjohn925@gmail.com>
2020-11-14 15:26:28 -0800

    Update jumptable.py

--- 06 Conditionals/jumptable.py
+++ 06 Conditionals/jumptable.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# jumptable.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
 
 class jumptable():
     jumptable = {}
