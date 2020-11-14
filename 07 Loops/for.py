7e908516e9b428260cbca8c315410941495d131a
zhjohn925 <zhjohn925@gmail.com>
2020-11-14 15:42:38 -0800

    Update for.py

--- 07 Loops/for.py
+++ 07 Loops/for.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-# for.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
 
 def main():
     fh = open('lines.txt')
+    #readlines() is called iterator, which takes one object at a time from sequence objects
+    #for loop works with iterator
+    # line is a variable to hold object each time
     for line in fh.readlines():
         print(line)
+        #print(line, end='')
+    #str is container, containing characters     
+    for s in 'string' :
+        print(s)
+    for n in [1, 2, 3, 4, 5]:
+        print(n, end=' ')
+        
 
 if __name__ == "__main__": main()
