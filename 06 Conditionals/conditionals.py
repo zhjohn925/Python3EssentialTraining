f5e818795593ba2274e0ffdfaa702b7a511df76a
zhjohn925 <zhjohn925@gmail.com>
2020-11-14 15:24:50 -0800

    Update conditionals.py

--- 06 Conditionals/conditionals.py
+++ 06 Conditionals/conditionals.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
-# conditionals.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+
+# if statement: only one of statement is executed
 
 def main():
-    print("This is the conditionals.py file.")
-
+    a, b = 0, 1
+    if a > b:
+        print('a is greater than b')
+    else :
+        print('a is less than or equal to b')
+    if a > b:
+        print('a is greater than b')
+    elif a == b :   #can be multiple of elif
+        print('a is equal to b')
+    else :
+        print('a is less than b')
+    v = 'a is greater than b' if a > b else 'a is less than or equal to b'
+    print(v)
+    #switch
+    choices = dict(
+        one = 'first',
+        two = 'second',
+        three = 'third',
+        four = 'fourth',
+        five = 'fifth'
+    )
+    v = 'five'
+    print(choices[v])   #no error handler
+    print(choices.get(v, 'other'))
+        
 if __name__ == "__main__": main()
