6935bf6c674c92152109291e331d3d7b87d7dc6e
zhjohn925 <zhjohn925@gmail.com>
2020-11-21 16:49:29 -0800

    Update regex.py

--- 09 Regexes/regex.py
+++ 09 Regexes/regex.py
@@ -1,14 +1,52 @@
 #!/usr/bin/python3
-# regex.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Gorup, LLC
+
+#Regular expressions are a very powerful method of matching patterns in text
+
 
 import re
 
 def main():
     fh = open('raven.txt')
     for line in fh:
-        if re.search('(Len|Neverm)ore', line):
+        if re.search('(Len|Neverm)ore', line):   # '|' like 'or'
+            print(line, end='')
+            
+def main1():
+    fh = open('raven.txt')
+    for line in fh:
+        match = re.search('(Len|Neverm)ore', line)   # '|' like 'or'
+        if match:  
+            print(match.group())    
+            
+# sub - substitute            
+def main2():
+    fh = open('raven.txt')
+    for line in fh:
+        print(re.sub('(Len|Neverm)ore', '###', line), end='')   # '|' like 'or'
+
+def main3():
+    fh = open('raven.txt')
+    for line in fh:
+        match = re.search('(Len|Neverm)ore', line)   # '|' like 'or'
+        if match:  
+            print(line.replace(match.group(), '###'), end='')           
+    
+#define pattern, then use it over and over again
+#pre-compile pattern provides more efficient way since only compile it once
+def main4():
+    fh = open('raven.txt')
+    #pattern = re.compile('(Len|Neverm)ore')
+    pattern = re.compile('(Len|Neverm)ore', re.IGNORECASE)
+    for line in fh:
+        if re.search(pattern, line): 
             print(line, end='')
 
+def main5():
+    fh = open('raven.txt')
+    #pattern = re.compile('(Len|Neverm)ore')
+    pattern = re.compile('(Len|Neverm)ore', re.IGNORECASE)
+    for line in fh:
+        if re.search(pattern, line): 
+            print(pattern.sub('###', line), end='')
+            
 if __name__ == "__main__": main()
