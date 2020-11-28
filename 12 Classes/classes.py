dfcee7d14f075ea1659f53f2109d5585a68dabac
zhjohn925 <zhjohn925@gmail.com>
2020-11-28 14:00:31 -0800

    Update classes.py

--- 12 Classes/classes.py
+++ 12 Classes/classes.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-# classes.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+
+# Classes are how you create your own objects
+# A class is the blueprint for an object
+# An object is an instance of a class
 
 class Duck:
+    def __init__(self, color='white') :   #constructor
+        #name with "_", this attribute used in local, 
+        #not access this variable outside the object
+        #use getter and setter
+        self._color = color   #_color attached to objects, not to the class
+    
     def quack(self):
         print('Quaaack!')
 
     def walk(self):
         print('Walks like a duck.')
+        
+    def setColor(self, color) :
+        self._color = color   
+        
+    def getColor(self) :
+        return self._color
 
 def main():
     donald = Duck()
-    donald.quack()
+    print(donald)
+    donald.quack()   #self is donald,  'donald' is passed magically
     donald.walk()
+    donald1 = Duck('blue')
 
 if __name__ == "__main__": main()
