0c040c186a1ce6a8a7c5ce7e744a7fcfc0dea2de
zhjohn925 <zhjohn925@gmail.com>
2020-12-05 16:30:31 -0800

    Update modules.py

--- 17 Modules/modules.py
+++ 17 Modules/modules.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
-# modules.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+
+# module reference:
+# http://docs.python.org/py3k/library/index.html 
+# provides liable, efficient, and rich modules
 
 import sys
 
 def main():
     print('Python version {}.{}.{}'.format(*sys.version_info))
+    print(sys.platform)  #operating system
+    
+    #implement in different platform
+    import os
+    print(os.name)
+    print(os.getenv('PATH')
+    print(os.getcwd())  #current working directory
+    print(os.urandom(25))
+    
+    import urllib.request
+    page = urllib.request.urlopen('http://bw.org/')
+    type(page)  #this is iterable object
+    for line in page: print(str(line, encoding = 'utf_8'), end='')
+          
+    import random
+    print(random.randint(1, 1000))
+    x = list(range(25))
+    print(x)
+    random.shuffle(x)
+    print(x)
+          
+    import datetime
+    now = datetime.datetime.now()
+    print(now)
+    print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+          
 
 if __name__ == "__main__": main()
