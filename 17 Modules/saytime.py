01e980ad62bc7124cf461f871d1692d57c13af90
zhjohn925 <zhjohn925@gmail.com>
2020-12-05 16:42:45 -0800

    Update saytime.py

--- 17 Modules/saytime.py
+++ 17 Modules/saytime.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# saytime.py by Bill Weinman [http://bw.org/]
-# created for Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Gorup, LLC
+
 import sys
 import time
 
