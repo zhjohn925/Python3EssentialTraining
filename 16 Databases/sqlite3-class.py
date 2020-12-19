e80173627f24d92446c88de504605694e45bcec6
zhjohn925 <zhjohn925@gmail.com>
2020-12-19 15:00:02 -0800

    Update sqlite3-class.py

--- 16 Databases/sqlite3-class.py
+++ 16 Databases/sqlite3-class.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# sqlite3-class.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+
 
 import sqlite3
 
