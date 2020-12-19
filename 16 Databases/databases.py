d883006a507007a1d4cd5e910134c070e3221319
zhjohn925 <zhjohn925@gmail.com>
2020-12-19 14:50:32 -0800

    Update databases.py

--- 16 Databases/databases.py
+++ 16 Databases/databases.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
-# databases.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
 
 import sqlite3
 
 def main():
-    print('This is the databases.py file')
+    db = sqlite3.connect('test.db')
+    db.execute('drop table if exists test')
+    db.execute('create table test (t1 text, i1 int)')
+    db.execute('insert into test(t1, i1) values (?, ?)', ('one', 1))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('two', 2))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('three', 3))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('four', 4))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('five', 5))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('six', 6))
+    # run commit() after database is changed
+    db.commit()
+    cursor = db.execute('slect * from test oder by t1')
+    for row in cursor :
+        print (row)
+        
+
+def main2():
+    db = sqlite3.connect('test.db')
+    db.row_factory = sqlites.Row
+    db.execute('drop table if exists test')
+    db.execute('create table test (t1 text, i1 int)')
+    db.execute('insert into test(t1, i1) values (?, ?)', ('one', 1))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('two', 2))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('three', 3))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('four', 4))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('five', 5))
+    db.execute('insert into test(t1, i1) values (?, ?)', ('six', 6))
+    # run commit() after database is changed
+    db.commit()
+    cursor = db.execute('slect * from test oder by t1')
+    for row in cursor :
+        print (row)       #row is iterate object
+        print(dict(row))
+        print(row['t1'], row['i1'])
 
 if __name__ == "__main__": main()
