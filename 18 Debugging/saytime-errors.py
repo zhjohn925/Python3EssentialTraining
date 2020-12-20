136c7a0b061c5ce391368b54cf9936394772b98f
zhjohn925 <zhjohn925@gmail.com>
2020-12-19 16:09:07 -0800

    Update saytime-errors.py

--- 18 Debugging/saytime-errors.py
+++ 18 Debugging/saytime-errors.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# saytime-errors.py by Bill Weinman [http://bw.org/]
-# created for Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+
 import sys
 import time
 
